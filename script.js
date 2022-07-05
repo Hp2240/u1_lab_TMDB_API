cfd1793fc8ad24a6bd683c76a17a149e0003b97c
Hyeree Park <hp2240@nyu.edu>
2022-07-04 22:21:11 -0400

    completed TMDB hw

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
 const button = document.querySelector('button')
 const titleInput = document.querySelector('input')
 const list = document.querySelector('.movie-list')
+const movieInfo = document.querySelector('.more-info')
+let createButton = document.createElement('button')
 
 button.addEventListener('click', async () => {
   let search = titleInput.value
@@ -14,13 +16,20 @@ button.addEventListener('click', async () => {
   )
   console.log(response)
   let movieList = response.data.results
+  let titleList = response.data.titles
   const renderList = (arr) => {
     console.log(renderList)
     arr.forEach((movie) => {
       let movies = document.createElement('div')
+      let titles = document.createElement('h2')
       movies.innerHTML = `<img src=https://image.tmdb.org/t/p/original${movie.poster_path}>`
+      titles.innerHTML = `${movie.title}`
       list.append(movies)
+      list.append(titles)
     })
   }
   renderList(movieList)
+  renderList(titles)
 })
+
+// movie.data.result.length
